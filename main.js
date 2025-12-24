e0cdd7a347c747aa33b8c16752981a50b15101fc
WALEED 912 <wi474600@gmail.com>
2025-12-24 13:20:08 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,166 +1,99 @@
-// Variables
-const prodContProd = document.querySelector("#products");
-const prodContIndex = document.querySelector("#prod-index");
-const prodContCart = document.querySelector("#prod-cart");
+/* =========================
+   MOBILE HAMBURGER MENU
+========================= */
+function hamburgerMenu() {
+  const links = document.getElementById("myLinks");
 
-const prod1 = {
-  imgSrc: "img/nayris-aquino-Lidm0GHUL-0-unsplash.jpg",
-  name: "Nikon D3200",
-  price: 599,
-};
-const prod2 = {
-  imgSrc: "img/mikedelta-zUnc4-eHw6E-unsplash.jpg",
-  name: "Pentax MZ-50",
-  price: 299,
-};
-const prod3 = {
-  imgSrc: "img/rohit-jawalkar-bZvX1kozeRg-unsplash.jpg",
-  name: "Canon E0S",
-  price: 699,
-};
-
-const arr = [
-  prod1,
-  prod2,
-  prod3,
-  prod1,
-  prod2,
-  prod3,
-  prod1,
-  prod2,
-  prod3,
-  prod1,
-  prod2,
-  prod3,
-];
-
-// Function to display products on products.html
-const displayProducts = () => {
-  for (let i = 0; i < arr.length; i++) {
-    // Get values
-    let imgSrc = arr[i].imgSrc;
-    let name = arr[i].name;
-    let price = arr[i].price;
-    const check = "prod";
-    createProd(imgSrc, name, price, check);
-  }
-};
-
-// Function to display products on index.html
-const displayProdIndex = () => {
-  for (let i = 0; i < 3; i++) {
-    // Get values
-    let imgSrc = arr[i].imgSrc;
-    let name = arr[i].name;
-    let price = arr[i].price;
-    const check = "index";
-    createProd(imgSrc, name, price, check);
-  }
-};
-
-const displayProdCart = () => {
-  for (let i = 0; i < 2; i++) {
-    // Get values
-    let imgSrc = arr[i].imgSrc;
-    let name = arr[i].name;
-    let price = arr[i].price;
-    createCartProd(imgSrc, name, price);
-  }
-};
-
-// Function to create elements
-const createProd = (imgSrc, name, price, check) => {
-  let divProd = document.createElement("div");
-  let imgProd = document.createElement("img");
-  let nameProd = document.createElement("h4");
-  let priceProd = document.createElement("p");
-  let buttonProd = document.createElement("button");
-  let divOverlay = document.createElement("div");
-  let prodDesc = document.createElement("p");
-  let buttonMore = document.createElement("button");
-
-  // Set values on elements
-  imgProd.src = imgSrc;
-  nameProd.innerText = name;
-  priceProd.innerText = "$" + price;
-  buttonProd.innerText = "Add to cart";
-  prodDesc.innerText =
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sunt quam alias soluta ad, labore quas velit rem dolorum eius cum laboriosam magni provident similique!";
-  buttonMore.innerText = "More info";
-
-  // Add classes on elements
-  priceProd.className = "price";
-  buttonProd.className = "atc-btn";
-  buttonMore.className = "rm-btn";
-  divOverlay.className = "overlay";
-  prodDesc.className = "description";
-  divProd.className = "img-products";
-
-  // Add elements to div
-  divOverlay.appendChild(prodDesc);
-  divOverlay.appendChild(buttonMore);
-  divOverlay.appendChild(buttonProd);
-  divProd.appendChild(imgProd);
-  divProd.appendChild(nameProd);
-  divProd.appendChild(priceProd);
-  divProd.appendChild(divOverlay);
-  if (check === "prod") {
-    prodContProd.appendChild(divProd);
-  } else if (check === "index") {
-    prodContIndex.appendChild(divProd);
-  }
-};
-
-const createCartProd = (imgSrc, name, price) => {
-  let divProd = document.createElement("div");
-  let imgProd = document.createElement("img");
-  let descProd = document.createElement("div");
-  let nameProd = document.createElement("h4");
-  let priceProd = document.createElement("p");
-  let amountDiv = document.createElement("div");
-  let plusIcon = document.createElement("i");
-  let minusIcon = document.createElement("i");
-  let amount = document.createElement("p");
-  let icons = document.createElement("div");
-  let closeIcon = document.createElement("p");
-  let favoriteIcon = document.createElement("i");
-
-  // Set values on elements
-  imgProd.src = imgSrc;
-  nameProd.innerText = name;
-  priceProd.innerText = "$" + price;
-  amount.innerText = " 1 ";
-
-  divProd.className = "cart-prod";
-  descProd.className = "desc-prod";
-  amountDiv.className = "amount-div";
-  plusIcon.className = "fa-regular fa-square-plus";
-  minusIcon.className = "fa-regular fa-square-minus";
-  icons.className = "cart-icons";
-  closeIcon.className = "fa-regular fa-rectangle-xmark";
-  favoriteIcon.className = "fa-solid fa-heart";
-
-  // Add elements to div
-  divProd.appendChild(imgProd);
-  descProd.appendChild(nameProd);
-  descProd.appendChild(priceProd);
-  amountDiv.appendChild(minusIcon);
-  amountDiv.appendChild(amount);
-  amountDiv.appendChild(plusIcon);
-  descProd.appendChild(amountDiv);
-  divProd.appendChild(descProd);
-  icons.appendChild(closeIcon);
-  icons.appendChild(favoriteIcon);
-  divProd.appendChild(icons);
-  prodContCart.appendChild(divProd);
-};
-
-// Function for mobile menu
-const hamburgerMenu = () => {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+  if (links.style.display === "block") {
+    links.style.display = "none";
   } else {
-    x.style.display = "block";
+    links.style.display = "block";
   }
-};
+}
+
+/* =========================
+   TRENDING PRODUCTS (INDEX)
+========================= */
+function displayProdIndex() {
+  const container = document.getElementById("prod-index");
+  if (!container) return;
+
+  const products = [
+    {
+      name: "Canon EOS R5",
+      price: "$3,899",
+      img: "img/camera1.jpg",
+    },
+    {
+      name: "Sony Alpha A7 IV",
+      price: "$2,499",
+      img: "img/camera2.jpg",
+    },
+    {
+      name: "Nikon Z6 II",
+      price: "$1,999",
+      img: "img/camera3.jpg",
+    },
+    {
+      name: "DJI Mini Drone",
+      price: "$599",
+      img: "img/drone.jpg",
+    },
+  ];
+
+  container.innerHTML = "";
+
+  products.forEach((product) => {
+    const card = document.createElement("div");
+    card.className = "product-card";
+
+    card.innerHTML = `
+      <img src="${product.img}" alt="${product.name}">
+      <h3>${product.name}</h3>
+      <p class="price">${product.price}</p>
+      <button class="buy-btn">Add to cart</button>
+    `;
+
+    container.appendChild(card);
+  });
+}
+
+/* =========================
+   SEARCH (BASIC DEMO)
+========================= */
+const searchInput = document.getElementById("search-input");
+
+if (searchInput) {
+  searchInput.addEventListener("keyup", () => {
+    const value = searchInput.value.toLowerCase();
+    console.log("Searching for:", value);
+    // Later you can filter products here
+  });
+}
+
+/* =========================
+   SUBSCRIBE BUTTON
+========================= */
+const subscribeBtn = document.getElementById("subscribe-btn");
+const subscribeInput = document.getElementById("subscribe-input");
+
+if (subscribeBtn) {
+  subscribeBtn.addEventListener("click", () => {
+    if (subscribeInput.value === "") {
+      alert("Please enter your email");
+    } else {
+      alert("Thank you for subscribing!");
+      subscribeInput.value = "";
+    }
+  });
+}
+
+/* =========================
+   CLOSE MOBILE MENU ON RESIZE
+========================= */
+window.addEventListener("resize", () => {
+  const links = document.getElementById("myLinks");
+  if (window.innerWidth > 768 && links) {
+    links.style.display = "none";
+  }
+});
